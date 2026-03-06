323d38975afe379eca10172278144d1a06203d89
VencelKocsis <katzarmondi@gmail.com>
2026-03-06 01:07:06 +0100

    name replaced with id

--- app/src/main/java/hu/bme/aut/android/demo/data/network/model/teamMatch/MatchParticipantDTO.kt
+++ app/src/main/java/hu/bme/aut/android/demo/data/network/model/teamMatch/MatchParticipantDTO.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MatchParticipantDTO(
     val id: Int,
+    val userId: Int,
     val playerName: String,
     val teamSide: String, // HOME or GUEST
     val status: String, // APPLIED or SELECTED
